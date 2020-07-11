19217f8fff2f447a3c654f6f25ef9744dc1722ee
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-07-11 23:08:51 +0900

    Impl CodeComp

--- projects/06/assembler/code.go
+++ projects/06/assembler/code.go
@@ -23,3 +23,70 @@ func CodeDest(mnemonic string) string {
 	// FIXME: error を返すようにした方がいいかも
 	return ""
 }
+
+// CodeComp returns binary code of comp mnemonic
+func CodeComp(mnemonic string) string {
+	switch mnemonic {
+	// when a=0
+	case "0":
+		return "0101010"
+	case "1":
+		return "0111111"
+	case "-1":
+		return "0111010"
+	case "D":
+		return "0001100"
+	case "A":
+		return "0110000"
+	case "!D":
+		return "0001101"
+	case "!A":
+		return "0110001"
+	case "-D":
+		return "0001111"
+	case "-A":
+		return "0110011"
+	case "D+1":
+		return "0011111"
+	case "A+1":
+		return "0110111"
+	case "D-1":
+		return "0001110"
+	case "A-1":
+		return "0110010"
+	case "D+A":
+		return "0000010"
+	case "D-A":
+		return "0010011"
+	case "A-D":
+		return "0000111"
+	case "D&A":
+		return "0000000"
+	case "D|A":
+		return "0010101"
+
+	// when a=1
+	case "M":
+		return "1110000"
+	case "!M":
+		return "1110001"
+	case "-M":
+		return "1110011"
+	case "M+1":
+		return "1110111"
+	case "M-1":
+		return "1110010"
+	case "D+M":
+		return "1000010"
+	case "D-M":
+		return "1010011"
+	case "M-D":
+		return "1000111"
+	case "D&M":
+		return "1000000"
+	case "D|M":
+		return "1010101"
+	}
+	// FIXME: error を返すようにした方がいいかも
+	return ""
+}
