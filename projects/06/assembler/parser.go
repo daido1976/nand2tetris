8e34aa067fc4316094bbbbf3cfc3bc17d15c26cc
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-07-11 21:24:28 +0900

    Remove fileOut

--- projects/06/assembler/parser.go
+++ projects/06/assembler/parser.go
@@ -43,24 +43,6 @@ func main() {
 	}
 }
 
-// test
-func fileOut() {
-	file, err := os.Open("./projects/06/add/Add.asm")
-	if err != nil {
-		fmt.Println("error")
-	}
-	defer file.Close()
-	fmt.Printf("name: %s\n", file.Name())
-
-	s := bufio.NewScanner(file)
-
-	// return to line 10
-	for i := 1; s.Scan() && i < 10; i++ {
-		line := s.Text()
-		fmt.Println(i, line)
-	}
-}
-
 func newParser(s *bufio.Scanner) *Parser {
 	return &Parser{s, ""}
 }
